2cff69b75023fc0abe552f6178556f0f8cf5b6d5
Victoria Hong <victoriaxhong@gmail.com>
2018-05-06 03:19:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 function shout(string) { return string.toUpperCase() }
 function whisper(string) {return string.toLowerCase() }
-function logWhisper(string) { console.log() string.toLowerCase }
+function logWhisper(string) { console.log('its one argument in all lowercase') string.toLowerCase }
