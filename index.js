caec4414cedf7d4a3da0c671d7ae39857b2415d9
Victoria Hong <victoriaxhong@gmail.com>
2018-05-06 03:17:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ function sayHiToGrandma(string) {
   else if ((string)===lowercase) {
     return "I can't hear you!"
   }
-  else if ((string)==="I love you, Grandma.")) {
+  else if ((string)==="I love you, Grandma.") {
     return "I love you, too." } }
